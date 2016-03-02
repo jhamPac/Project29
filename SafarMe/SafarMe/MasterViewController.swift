960792ea1cb3334f2a4df437b5a565f7b9832e73
Kevia <cloud@kevia.me>
2016-03-02 13:10:30 -0800

    Added jazzy for clean documentation

--- SafarMe/SafarMe/MasterViewController.swift
+++ SafarMe/SafarMe/MasterViewController.swift
@@ -11,8 +11,17 @@ import SafariServices
 
 typealias Projects = [(title: String, description: String)]
 
+/**
+    The master view for UISplitViewController
+*/
+
 class MasterViewController: UITableViewController
 {
+    /**
+        The projects the user can view
+     
+        - Remark: Each project has an integer attached to it. This integer is used in showTutorial() function to segue the user to the appropriate URL
+    */
     var projects: Projects = [
         (title: "Project 1: Storm Viewer", description: "Constants and variables, UITableView, UIImageView, NSFileManager, storyboards"),
         (title: "Project 2: Guess the Flag", description: "@2x and @3x images, asset catalogs, integers, doubles, floats, operators (+= and -=), UIButton, enums, CALayer, UIColor, random numbers, actions, string interpolation, UIAlertController"),
@@ -38,6 +47,16 @@ class MasterViewController: UITableViewController
     
     // MARK: - VC Methods
     
+    /**
+        Creates a NSAttributed String
+        
+        - Parameter title: A string for the title of a project
+    
+        - Parameter subtitle: A string for the description of a project
+    
+        - Returns: NSAtributedString created by combining the title and subtitle
+    */
+    
     func makeAttributedString(title title: String, subtitle: String) -> NSAttributedString
     {
         let titleAttributes = [NSFontAttributeName: UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline), NSForegroundColorAttributeName: UIColor.purpleColor()]
@@ -49,6 +68,19 @@ class MasterViewController: UITableViewController
         titleString.appendAttributedString(subtitleString)
         
         return titleString
+    }
+    
+    /**
+        Opens a SFSariViewController with the supplied Integer.
+     
+        - Parameter which: An Int that identifies which URL to use for the project
+     
+        - Returns: Void
+    */
+    
+    func showTutorial(which: Int)
+    {
+        
     }
 
     // MARK: - Table View
